0f306ba8c95444ad4f60e3864172de2bd320af81
Administrator <Administrator@Quality-PC>
2022-09-14 14:21:05 +0530

    [vino]

---
+++ src/vaibhav/demo.java
@@ -0,0 +1,11 @@
+package vaibhav;
+
+public class demo 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("code 2 is running");
+		
+	}
+
+}
---
+++ src/vaibhav/demo1.java
@@ -0,0 +1,11 @@
+package vaibhav;
+
+public class demo1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("code 1 is running");
+		
+	}
+
+}
